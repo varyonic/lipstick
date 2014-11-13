4f23b44440d786e39019cbdad27da0f940783756
Piers Chambers <piers@varyonic.com>
2014-11-13 13:36:30 -0500

    Verify that validate_credentials rejects bad password.

--- test/test_lipstick.rb
+++ test/test_lipstick.rb
@@ -6,7 +6,7 @@ describe 'Lipstick::Api::Session' do
   before(:each) do
     @session = Lipstick::Api::Session.new(fixtures(:credentials))
   end
-  	
+
   describe '#shipping_method_find' do
     it "finds shipping methods" do
       api_response = @session.shipping_method_find
@@ -17,6 +17,12 @@ describe 'Lipstick::Api::Session' do
   describe "validate_credentials" do
     it "returns true if credentials valid" do
       assert @session.validate_credentials == true, "Credentials not valid."
+    end
+
+    it "returns false if credentials invalid" do
+      invalid_credentials = fixtures(:credentials).merge!(password: 'invalid')
+      @invalid_session = Lipstick::Api::Session.new(invalid_credentials)
+      assert @invalid_session.validate_credentials == false, "Invalid credentials not detected."
     end
   end
 end
